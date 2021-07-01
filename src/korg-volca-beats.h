c1072af17eaff5c281bccdccf4d312ce608e67f8
montoyamoraga <montoyamoraga@gmail.com>
2021-07-01 15:23:02 -0400

    add korg volca beats draft

---
+++ src/korg-volca-beats.h
@@ -0,0 +1,15 @@
+#ifndef MAQUINITAS_PARAMETERS_KORG_VOLCA_BEATS_H
+#ifndef MAQUINITAS_PARAMETERS_KORG_VOLCA_BEATS_H
+
+class KorgVolcaBeats {
+  public:
+    int noteBassDrum = 36;
+    int noteSnare = 38;
+    int noteClap = 39;
+    int noteClosedHiHat = 42;
+    int noteLowTom = 43;
+    int noteOpenHiHat = 46;
+    int noteHiTom = 50;
+}
+
+#endif
