9cbb679f09fc304396fc5b8f43b347d4dc9e2737
aaron montoya-moraga <montoyamoraga@gmail.com>
2021-10-28 03:01:43 -0300

    add draft of CC

--- src/korg/KorgVolcaFm.h
+++ src/korg/KorgVolcaFm.h
@@ -12,6 +12,18 @@ class KorgVolcaFm: public MaquinitasParams {
 
     int midiChannel = 1;
 
+    int ccTranspose = 40;
+    int ccVelocity = 41;
+    int ccModulatorAttack = 42;
+    int ccModulatorDecay = 43;
+    int ccCarrierAttack = 44;
+    int ccCarrierDecay = 45;
+    int ccLfoRate = 46;
+    int ccLfoPitchDepth = 47;
+    int ccAlgorithm = 48;
+    int ccArpType = 49;
+    int ccArpDiv = 50;
+
 };
 
 #endif
